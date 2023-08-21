1905462f7d6f6ea7a5935426a84c7722c1c5752a
WatermelonD <12351801+watermelond@user.noreply.gitee.com>
2023-08-22 00:55:56 +0800

    完整demo

--- zhiliaooa/app.py
+++ zhiliaooa/app.py
@@ -1,37 +1,48 @@
-from flask import Flask
+from flask import Flask, session, g
 import config
-from exts import db
+from exts import db, mail
 from models import UserModel
 from blueprints.qa import bp as qa_bp
 from blueprints.auth import bp as auth_bp
 from flask_migrate import Migrate
 
-app = Flask(__name__)
-## 绑定配置文件
-app.config.from_object(config)
-## 可以先创建db，再绑定app
-db.init_app(app)
 
-## ORM模型映射成表
+app = Flask(__name__)
+# 绑定配置文件
+app.config.from_object(config)
+
+db.init_app(app)
+mail.init_app(app)
+
 migrate = Migrate(app, db)
 
-## ORM模型映射成表三部曲(终端terminal输入)
-# 1.flask db init ## 这步只需要执行一次(下次更新表，不用再执行，初始化后生成migrations文件夹后就不用再初始化了)
-# 2.flask db migrate  ## 识别ORM模型的改变，生成迁移脚本
-# 3.flask db upgrade  ## 运行迁移脚本，同步到数据库中
-
-
-## app绑定蓝图
 app.register_blueprint(qa_bp)
 app.register_blueprint(auth_bp)
 
-## blueprint 蓝图：用来做模块化的
-## 比如豆瓣网，有电影、读书、音乐、xx ,每个模块都可以定义成一个蓝图，然后和电影相关的视图函数全部放进电影蓝图中，
-## 视图函数全部放到蓝图中
+# blueprint：用来做模块化的
+# 电影、读书、音乐、xxx
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello World!'
+# flask db init：只需要执行一次
+# flask db migrate：将orm模型生成迁移脚本
+# flask db upgrade：将迁移脚本映射到数据库中
+
+# before_request/ before_first_request/ after_request
+# hook
+@app.before_request
+def my_before_request():
+    user_id = session.get("user_id")
+    if user_id:
+        user = UserModel.query.get(user_id)
+        setattr(g, "user", user)
+    else:
+        setattr(g, "user", None)
+
+
+@app.context_processor
+def my_context_processor():
+    return {"user": g.user}
+
+
 
 
 if __name__ == '__main__':
