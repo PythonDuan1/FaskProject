b2bc1c6e3757a261eaa6d978bfecaee1a6962509
WatermelonD <12351801+watermelond@user.noreply.gitee.com>
2023-08-24 11:17:20 +0800

    0823更新

--- zhiliaooa/app.py
+++ zhiliaooa/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, g
+from flask import Flask
 import config
 from exts import db, mail
 from models import UserModel
@@ -6,43 +6,33 @@ from blueprints.qa import bp as qa_bp
 from blueprints.auth import bp as auth_bp
 from flask_migrate import Migrate
 
-
 app = Flask(__name__)
-# 绑定配置文件
+## 绑定配置文件
 app.config.from_object(config)
-
+## 可以先创建db，再绑定app
 db.init_app(app)
 mail.init_app(app)
 
+## ORM模型映射成表
 migrate = Migrate(app, db)
 
+## ORM模型映射成表三部曲(终端terminal输入)
+# 1.flask db init ## 这步只需要执行一次(下次更新表，不用再执行，初始化后生成migrations文件夹后就不用再初始化了)
+# 2.flask db migrate  ## 识别ORM模型的改变，生成迁移脚本
+# 3.flask db upgrade  ## 运行迁移脚本，同步到数据库中
+
+
+## app绑定蓝图
 app.register_blueprint(qa_bp)
 app.register_blueprint(auth_bp)
 
-# blueprint：用来做模块化的
-# 电影、读书、音乐、xxx
+## blueprint 蓝图：用来做模块化的
+## 比如豆瓣网，有电影、读书、音乐、xx ,每个模块都可以定义成一个蓝图，然后和电影相关的视图函数全部放进电影蓝图中，
+## 视图函数全部放到蓝图中
 
-# flask db init：只需要执行一次
-# flask db migrate：将orm模型生成迁移脚本
-# flask db upgrade：将迁移脚本映射到数据库中
-
-# before_request/ before_first_request/ after_request
-# hook
-@app.before_request
-def my_before_request():
-    user_id = session.get("user_id")
-    if user_id:
-        user = UserModel.query.get(user_id)
-        setattr(g, "user", user)
-    else:
-        setattr(g, "user", None)
-
-
-@app.context_processor
-def my_context_processor():
-    return {"user": g.user}
-
-
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return 'Hello World!'
 
 
 if __name__ == '__main__':
--- zhiliaooa/exts.py
+++ zhiliaooa/exts.py
@@ -1,6 +1,4 @@
-# exts.py：这个文件存在的意义就是为了解决循环引用的问题
-
-# flask-sqlalchemy
+## exts.py 这个文件存在的意义是为了解决循环引用的问题
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 
